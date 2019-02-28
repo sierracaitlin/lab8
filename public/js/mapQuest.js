b7d48b5c0ff03e2f4b7389de2c1fb5cb4ce3b6ef
Sierra Gutierrez <scg003@ucsd.edu>
2019-02-28 11:35:54 -0800

    added maps, load bio, and rsvp form

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = 'NKM57IIU2AX2ASMlgESlB7VAsu86kzMd';
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.8788, -117.2359],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  // Add a marker to the map
+  L.marker([32.8788, -117.2359]).addTo(map);
 }
